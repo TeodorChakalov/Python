cc2c984ff45015a9a7c50e291d8931a14453ad07
Teodor Chakalov <teko_sz@abv.bg>
2022-03-03 00:47:21 +0200

    .

---
+++ Python_Basics/Conditional_Statements_Exercise/Bonus_Score.py
@@ -0,0 +1,18 @@
+number = int(input())
+
+if number <= 100:
+    bonus = 5
+elif 100 < number <= 1000:
+    bonus = number / 5
+elif number > 1000:
+    bonus = number / 10
+
+if number % 2 == 0:
+    bonus += 1
+if number % 10 == 5:
+    bonus += 2
+
+sum = number + bonus
+
+print(bonus)
+print(sum)
