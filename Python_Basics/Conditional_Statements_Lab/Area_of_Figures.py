86eafa3b70117f1f5230058497bf930e57b21e62
Teodor Chakalov <teko_sz@abv.bg>
2022-03-02 23:49:29 +0200

    .

---
+++ Python_Basics/Conditional_Statements_Lab/Area_of_Figures.py
@@ -0,0 +1,21 @@
+from math import pi
+import math
+
+figure = input()
+
+if figure == "square":
+    a = float(input())
+    area = a * a
+elif figure == "rectangle":
+    a = float(input())
+    b = float(input())
+    area = a * b
+    # print(round(area, 3))
+elif figure == "circle":
+    r = float(input())
+    area = pi * r * r
+elif figure == "triangle":
+    a = float(input())
+    h = float(input())
+    area = a * h / 2
+print(f"{area:.3f}")
